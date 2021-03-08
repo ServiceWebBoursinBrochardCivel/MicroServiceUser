664589246628c65e66dcefe06a314ed7602cbcff
zehyfr <zehyfrab@gmail.com>
2021-03-08 17:15:55 +0100

    ok

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from Router.userApi import user_api
-from Router.ConnectApi import search_api
+from Router.ConnectApi import connect_api
 
 app=Flask(__name__)
 CORS(app)
