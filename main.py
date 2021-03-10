5e9ac6da29550af2808395c9d27faf693037f73e
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2021-03-10 17:39:48 +0100

    creation of the swagger documentation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 from flask_cors import CORS
 from Router.userApi import user_api
 from Router.ConnectApi import connect_api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app=Flask(__name__)
 CORS(app)
@@ -9,6 +10,23 @@ CORS(app)
 
 app.register_blueprint(user_api)
 app.register_blueprint(connect_api)
+
+
+@app.route('/Router/<path:path>')
+def send_api(path) :
+    return send_from_directory('Router',path)
+
+
+SWAGGER_URL = '/spec'
+API_URL = '/Router/swagger.json'
+swaggerui_api = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name' : "UsersApi"
+    }
+)
+app.register_blueprint(swaggerui_api,url_prefix=SWAGGER_URL)
 
 if __name__ =='__main__' :
     app.run(port=5001)
